f84fea4c608729bc7c6f70e05aeaeb872ba9c7a4
jwilkinson6179 <JamesW6179@gmail.com>
2020-03-18 13:08:59 -0400

    Switching to AWS

--- src/main/java/daos/GameDao.java
+++ src/main/java/daos/GameDao.java
@@ -7,8 +7,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.logging.Logger;
 
 import static java.util.logging.Level.INFO;
@@ -18,37 +18,35 @@ public class GameDao implements MyDao<Game> {
 
     private static final Logger LOGGER = Logger.getLogger(GameDao.class.getName());
 
-    public GameDao()
-    {
+    public GameDao() {
     }
 
-    public Game findById(Integer id)
-    {
+    public Game findById(Integer id) {
         Connection connection = null;
 
         try {
             connection = ConnectionFactory.getConnection();
-            Statement stmt = connection.createStatement();
+            Statement stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             ResultSet rs = stmt.executeQuery("SELECT * FROM games");
-            while(rs.next())
-            {
-                if(rs.getInt("id") == id)
-                {
+
+            rs.beforeFirst();
+            while (rs.next()) {
+                if (rs.getInt("id") == id) {
                     Game result = new Game(rs.getInt("id"), rs.getString("title"),
                             rs.getString("publisher"), rs.getString("genre"),
                             rs.getString("platform"), rs.getDate("release_date"),
                             rs.getString("franchise"));
 
+                    stmt.close();
+                    rs.close();
                     return result;
                 }
             }
-        } catch (SQLException e)
-        {
+        } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             try {
-                if (connection != null)
-                {
+                if (connection != null) {
                     connection.close();
                 }
                 LOGGER.log(INFO, ANSI_CYAN + "Connection closed.");
@@ -60,9 +58,41 @@ public class GameDao implements MyDao<Game> {
         return null;
     }
 
-    public List findAll()
+    public ArrayList<Game> findAll()
     {
-        return null;
+        Connection connection = null;
+        ArrayList<Game> gameList = new ArrayList<>();
+
+        try {
+            connection = ConnectionFactory.getConnection();
+            Statement stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ResultSet rs = stmt.executeQuery("SELECT * FROM games");
+
+            rs.beforeFirst();
+            while (rs.next()) {
+                Game entry = new Game(rs.getInt("id"), rs.getString("title"),
+                        rs.getString("publisher"), rs.getString("genre"),
+                        rs.getString("platform"), rs.getDate("release_date"),
+                        rs.getString("franchise"));
+
+                gameList.add(entry);
+            }
+            stmt.close();
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+                LOGGER.log(INFO, ANSI_CYAN + "Connection closed.");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return gameList;
     }
 
     public Game update(Game dto)
